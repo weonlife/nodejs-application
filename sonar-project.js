b928c284c291d511823835dbdddfbf2993757cea
devopscalgary <93885659+devopscalgary@users.noreply.github.com>
2022-02-27 11:25:18 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.131.162.209:5000/',
+  serverUrl: 'http://3.131.162.209:500/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
